7d57efa7eb74c7821f09d35dc675e6de902a1e37
CyrilYMZ <114634763+CyrilYMZ@users.noreply.github.com>
2023-02-21 14:53:02 -0500

    Update initialization.py

--- initialization.py
+++ initialization.py
@@ -1,8 +1,8 @@
 # imports
-
+import numpy as np
 # function
 
-def initialize_virus_population (virus_pop_size):
-    virus_pop = []
+def initialize_virus_population (virus_pop, virus_pop_size):
+    virus_pop = np.zeros((virus_pop, virus_size))
 
     return virus_pop
